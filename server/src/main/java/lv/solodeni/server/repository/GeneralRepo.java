9e2bbe11eb9315640aa6a5c4fc96b068b0f5e52c
Deniss Solovjovs <deniss11sol@gmail.com>
2025-01-10 02:48:53 +0200

    code refined

--- server/src/main/java/lv/solodeni/server/repository/GeneralRepo.java
+++ server/src/main/java/lv/solodeni/server/repository/GeneralRepo.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -17,12 +18,19 @@ public class GeneralRepo {
 
     private final NamedParameterJdbcTemplate template;
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
     public List<String> getAllTableNames() {
+        String dbName = dataSourceUrl.replace("jdbc:mysql://localhost:3307/", "")
+                .replace("?createDatabaseIfNotExist=true", "");
+
         String sql = """
                 SELECT table_name
                 FROM information_schema.tables
-                WHERE table_type = 'BASE TABLE' AND TABLE_SCHEMA = 'db';
-                """;
+                WHERE table_type = 'BASE TABLE' AND TABLE_SCHEMA = '%db_name%';
+                """.replace("%db_name%", dbName);
+
         return template.query(sql, (rs, rowNum) -> rs.getString("table_name"));
     }
 
